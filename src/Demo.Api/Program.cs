5b6c1dd2b63d0537590fbf9f577738848692ef8a
geogiam <morganaforlife@yandex.com>
2026-09-10 15:52:28 +0100

    clean DI

--- src/Demo.Api/Program.cs
+++ src/Demo.Api/Program.cs
@@ -1,50 +1,52 @@
 using Demo.Api.Endpoints;
-using Demo.Application.Interfaces;
 using Demo.Api.Startup;
-using Demo.Infrastructure.SqlRepository.Repositories;
-using Demo.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+// if we had settings, we would test them here at start to make sure they exists and not have the app crash at runtime.
+// SettingsTester.TestSettingsExist(builder.Configuration);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddScoped<IShiftRepository, ShiftRepository>();
-builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-builder.Services.AddScoped<IShiftService, ShiftService>();
-builder.Services.AddInMemoryDbContext();
+// if we had any
+// builder.Services.AddAuthorization(builder.Configuration);
 
+builder.Services.AddDatabase(builder.Configuration);
+builder.Services.AddRepositories();
+builder.Services.AddServices();
+
+// if we had them
+// builder.Services.AddWorkers(builder.Configuration);
+
+// if we needed it
+// builder.Services.AddHttpClient();                    
+
+#if DEBUG
 builder.Services.AddProblemDetails();
+#endif
 
 var app = builder.Build();
-
-// not needed for this demo, but in production you should use https and authorization
-// app.UseHttpsRedirection();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-
     app.UseSwagger();
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
+        options.RoutePrefix = "swagger";
     });
 }
 
-// UseStatusCodePages enables middleware that provides default responses for HTTP status codes
-// (like 404, 400, 500) when your API does not return a body
+// if we had any
+// app.UseAuthentication();
+// app.UseAuthorization();
+
 app.UseStatusCodePages();
 app.UseExceptionHandler();
 
 app.MapEmployeeEndpoints();
 app.MapShiftEndpoints();
-
-// not needed for this demo, but in production you should use https and authorization
-// app.UseAuthorization(); 
 
 app.Run();
---
+++ src/Demo.Api/Startup/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Demo.Application.Interfaces;
+using Demo.Application.Services;
+using Demo.Infrastructure.SqlRepository.Data;
+using Demo.Infrastructure.SqlRepository.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Api.Startup;
+
+public static class DependencyInjection
+{
+    public const string DatabaseName = "DemoDb";
+
+    public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase(DatabaseName));
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IShiftRepository, ShiftRepository>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IEmployeeService, EmployeeService>();
+        services.AddScoped<IShiftService, ShiftService>();
+
+        return services;
+    }
+
+    // public static IServiceCollection AddWorkers .....
+}
